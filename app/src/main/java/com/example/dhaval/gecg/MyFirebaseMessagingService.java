2f37a542e9f7335caf9630b8a608cf9673433a58
Hardik <hardik.patel4043@gmail.com>
2018-02-25 14:21:24 +0530

    notification completed

--- app/src/main/java/com/example/dhaval/gecg/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/dhaval/gecg/MyFirebaseMessagingService.java
@@ -1,20 +1,51 @@
 package com.example.dhaval.gecg;
 
-import android.app.Service;
-
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by Hardik on 2/24/2018.
  */
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+        String notificationTitle=remoteMessage.getData().get("title").toString();
+        String body=remoteMessage.getData().get("body");
+
+        int mNotificationId = (int)System.currentTimeMillis();
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                        .setAutoCancel(true)
+                        .setContentTitle(notificationTitle)
+                        .setContentText(body);
+
+        Intent resultIntent = new Intent(this,HomePage.class);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotifyMgr =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (mNotifyMgr != null) {
+            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        }
 
     }
+
 }
